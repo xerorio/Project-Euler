a45962ca9afe15a04600e42056d2a08034af2516
xerorio <lucakchan@gmail.com>
2021-09-20 14:22:01 +0100

    I don't know what to write

--- 0009.py
+++ 0009.py
@@ -4,15 +4,18 @@
 # There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 # Find the product abc.
 
-for i in range(0, 1000):
-    for j in range(0, 1000):
-        for k in range(0, 1000):
-            if (i + j + k == 1000):
-                if (i**2 + j**2 == k**2):
-                    print(i*j*k)
-                elif (i**2 == j**2 + k**2):
-                    print(i*j*k)
-                elif (i**2 + k**2 == j**2):
-                    print(i*j*k)
+def brute_force():
+    for i in range(0, 1000):
+        for j in range(0, 1000):
+            for k in range(0, 1000):
+                if (i + j + k == 1000):
+                    if (i**2 + j**2 == k**2):
+                        print(i*j*k)
+                    elif (i**2 == j**2 + k**2):
+                        print(i*j*k)
+                    elif (i**2 + k**2 == j**2):
+                        print(i*j*k)
+
+brute_force()
 
 # Answer: 31875000
--- 0039.py
+++ 0039.py
@@ -2,26 +2,6 @@
 # {20,48,52}, {24,45,51}, {30,40,50}
 # For which value of p ≤ 1000, is the number of solutions maximised?
 
-from math import sqrt
 
-pythagorean_triples = []
-perimeters = {}
-
-# generate_pythagorean_triples
-for i in range(1, 100):
-    denominator = i + i + 1
-    numerator = (denominator * i) + i
-    hypotenuse = sqrt( (denominator*denominator) + (numerator*numerator) )
-
-    triple = [denominator, numerator, int(hypotenuse)]
-    pythagorean_triples.append(triple)
-
-for triple in pythagorean_triples:
-    if sum(triple) in perimeters:
-        perimeters[sum(triple)].append(triple)
-    else:
-        perimeters[sum(triple)] = [triple,]
-
-print(perimeters)
 
 # Answer: 
