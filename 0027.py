c36d1f1b27c9b97d91c98efdd2e24021ae41a430
xerorio <lucakchan@gmail.com>
2022-07-12 16:15:11 +0100

    delete the stuff where im redoing it

--- 0027.py
+++
@@ -1,8 +0,0 @@
-# For quadratics in the form n^2 + an + b, where
-# modulus(a) < 1000 and modulus(b) < 1000
-
-# Find the product of the coefficients, a and b,
-# for the quadratic expression that produces the maximum number of
-# primes for consecutive values of n, starting with n = 0.
-
-# Answer: 
