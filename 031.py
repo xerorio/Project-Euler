e38b3f4fed0fe79266be88254b17f1f1783f45f0
xerorio <lucakchan@gmail.com>
2022-07-15 12:58:34 +0100

    move comments

--- 031.py
+++ 031.py
@@ -24,18 +24,12 @@ start = time()
 # 1 for £2 coins
 count = 1
 
-# 100p coins
-for a in range(3):
-    # 50p coins
-    for b in range( int(1 + ((200 - (100 * a)) / 50)) ):
-        # 20p coins
-        for c in range( int(1 + ((200 - (100 * a) - 50 * b) / 20)) ):
-            # 10p coins
-            for d in range( int(1 + ((200 - (100 * a) - (50 * b) - (20 * c)) / 10)) ):
-                # 5p coins
-                for e in range( int(1 + ((200 - (100 * a) - (50 * b) - (20 * c) - (10 * d)) / 5)) ):
-                    # 2p coins
-                    for f in range( int(1 + ((200 - (100 * a) - (50 * b) - (20 * c) - (10 * d) - (5 * e)) / 2)) ):
+for a in range(3): # 100p coins
+    for b in range( int(1 + ((200 - (100 * a)) / 50)) ): # 50p coins
+        for c in range( int(1 + ((200 - (100 * a) - (50 * b)) / 20)) ): # 20p coins
+            for d in range( int(1 + ((200 - (100 * a) - (50 * b) - (20 * c)) / 10)) ): # 10p coins
+                for e in range( int(1 + ((200 - (100 * a) - (50 * b) - (20 * c) - (10 * d)) / 5)) ): # 5p coins
+                    for f in range( int(1 + ((200 - (100 * a) - (50 * b) - (20 * c) - (10 * d) - (5 * e)) / 2)) ): # 2p coins
                         count += 1 # the rest are made by 1p coins
 
 print(count)
