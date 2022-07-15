8b41d75ee74e4fecf229f68ae059c88a1d963dd8
xerorio <lucakchan@gmail.com>
2022-07-15 12:32:05 +0100

    add spaces

--- 031.py
+++ 031.py
@@ -26,15 +26,15 @@ count = 1
 # 100p coins
 for a in range(3):
     # 50p coins
-    for b in range(int(1 + ((200 - (100 * a)) / 50))):
+    for b in range( int(1 + ((200 - (100 * a)) / 50)) ):
         # 20p coins
-        for c in range(int(1 + ((200 - (100 * a) - 50 * b) / 20))):
+        for c in range( int(1 + ((200 - (100 * a) - 50 * b) / 20)) ):
             # 10p coins
-            for d in range(int(1 + ((200 - (100 * a) - (50 * b) - (20 * c)) / 10))):
+            for d in range( int(1 + ((200 - (100 * a) - (50 * b) - (20 * c)) / 10)) ):
                 # 5p coins
-                for e in range(int(1 + ((200 - (100 * a) - (50 * b) - (20 * c) - (10 * d)) / 5))):
+                for e in range( int(1 + ((200 - (100 * a) - (50 * b) - (20 * c) - (10 * d)) / 5)) ):
                     # 2p coins
-                    for f in range(int(1 + ((200 - (100 * a) - (50 * b) - (20 * c) - (10 * d) - (5 * e)) / 2))):
+                    for f in range( int(1 + ((200 - (100 * a) - (50 * b) - (20 * c) - (10 * d) - (5 * e)) / 2)) ):
                         count += 1
 
 print(count)
