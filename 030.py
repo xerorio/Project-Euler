10e57304a3a8efbd6bbbe3e04e2b80fcd798e351
xerorio <lucakchan@gmail.com>
2022-07-15 12:17:55 +0100

    lower for loop range

--- 030.py
+++ 030.py
@@ -12,7 +12,7 @@ from time import time
 start = time()
 
 total = 0
-for i in range(2, 355000):
+for i in range(2, 300000):
     if i == sum((int(n) ** 5) for n in str(i)):
         total += i
 print(total)
